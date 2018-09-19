4248699daffa3d94ef007d71064e938a749d14e7
lino <medolino@gmail.com>
2018-09-19 22:01:09 +0200

    db export functionality added

---
+++ test/schema/test-export-collection.js
@@ -0,0 +1,46 @@
+import test from 'ava'
+
+const mongoose = require('mongoose')
+const path = require('path')
+
+const schemaHelper = require(path.resolve('./lib/schema'))
+
+const Schema = mongoose.Schema
+
+test('export database schemas created on mongoose instance', t => {
+  const FirstSchema = new Schema({ 
+    name: 'string'
+  })
+
+  const SecondSchema = new Schema({ 
+    name: 'string'
+  })
+
+  const firstModel = mongoose.model('firstModel', FirstSchema)
+  const secondModel = mongoose.model('secondModel', SecondSchema)
+
+  const exportedSchema = schemaHelper.exportDatabaseSchema(mongoose)
+
+  const expectedSchema = {
+    firstModel: {
+      schema: {
+        name: { type: 'String' }
+      }
+    },
+    secondModel: {
+      schema: {
+        name: { type: 'String' }
+      }
+    }
+  }
+
+  t.deepEqual(exportedSchema, expectedSchema, 'exportedSchema does not match expectedSchema')
+})
+
+test('throw error while exporting schemas when method parameter is not mongooseInstance', t => {
+  const error = t.throws(() => {
+    schemaHelper.exportDatabaseSchema('notMongooseInstance');
+  }, TypeError)
+
+  t.is(error.message, 'mongooseInstance should be an instance of mongoose');
+})
